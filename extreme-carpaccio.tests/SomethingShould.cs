691b734bf49f8026a4220227b734c8c5c9e99e54
Villand <p1303207@IUTDOUAB2PC4.univ-lyon1.fr>
2016-06-22 12:01:32 +0200

    Création d'un test de validation de la formule

--- extreme-carpaccio.tests/SomethingShould.cs
+++ extreme-carpaccio.tests/SomethingShould.cs
@@ -10,8 +10,18 @@ namespace extreme_carpaccio.tests
     public class SomethingShould
     {
         [Test]
-        public void DoSomething()
+        public void VerificationFormule()
         {
+            double tax = 10;
+            double reduc = 0.5;
+            double entree = 15;
+            double sortie = 0;
+
+            sortie = entree*tax;
+            sortie = sortie * (1 - reduc);
+
+            Assert.IsTrue(sortie == 75);
+
             Assert.IsFalse(false);
         }
     }
